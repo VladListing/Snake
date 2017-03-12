c7f9b347f349e24b0dfc366baefc192279f17577
VladListing <chvch@mail.ru>
2017-03-12 17:47:32 +0700

    Пример реализации принципа инкапсуляции (Сокрытия)

--- ConsoleApplication1/ConsoleApplication1/Point.cs
+++ ConsoleApplication1/ConsoleApplication1/Point.cs
@@ -10,15 +10,34 @@ namespace ConsoleApplication1
     {
 
 
-        //------------------------поля класса "Point"----------------------------
+        //пппппппппппппппппппппппппппппппп поля класса "Point" пппппппппппппппппппппппппп
         public int X;
 
         public int Y;
 
         public char sym;
-        //-----------------------------------------------------------------------
+        //пппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппппп
 
-        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% процедура вывода заданного символа в заданной координате %%%%%%%%%%(Метод, функция , класса "Point" )%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        //KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK конструктор для класса "Point" KKKKKKKKK(пустой - по умолчанию )KKKKKKK(вариант-1)KKKKKKKKKKKKKKKKKKKK
+        //public Point()
+        //{
+
+        //}
+        //KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+        //KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK конструктор для класса "Point" KKKKKKKKK(с присвоением )KKKKKKKKK(вариант-2)KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+        public Point(int _X, int _Y, char _sym )
+        {
+            X = _X;
+            Y = _Y;
+          sym = _sym;
+
+        }
+        //KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+
+
+        //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM процедура вывода заданного символа в заданной координате %%%%%%%%%%(Метод, функция , класса "Point" )MMMMMMMMMMMMMMMMMMMMMMMMMMMMM
         //public void Draww(int X, int Y, char sym)
         public void Draww()
         {
@@ -27,7 +46,7 @@ namespace ConsoleApplication1
             Console.Write(sym); //выводим курсор на экран в заданных координатах
 
         }
-        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
     }
 }
