679c774889c3ac5f7b36f51a8bb486ccd60da82b
VladListing <chvch@mail.ru>
2017-03-12 12:57:52 +0700

    Создание класса "Point"

---
+++ ConsoleApplication1/ConsoleApplication1/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Point   // класс "Point"
+    {
+
+
+        //------------------------поля класса "Point"----------------------------
+        public int X;
+
+        public int Y;
+
+        public char sym;
+        //-----------------------------------------------------------------------
+
+        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% процедура вывода заданного символа в заданной координате %%%%%%%%%%(Метод, функция , класса "Point" )%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        //public void Draww(int X, int Y, char sym)
+        public void Draww()
+        {
+
+            Console.SetCursorPosition(X, Y);//задаем координаты курсора
+            Console.Write(sym); //выводим курсор на экран в заданных координатах
+
+        }
+        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    }
+}
