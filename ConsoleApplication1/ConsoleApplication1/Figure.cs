50fc3cfc3c764b127aadc0f36d489b682fd683b4
VladListing <chvch@mail.ru>
2017-03-18 20:56:03 +0700

    класс предок "Figure"

---
+++ ConsoleApplication1/ConsoleApplication1/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Figure
+    {
+
+         protected List<Point> pList; //обьявляем список "pList" состоящий из обьектов класса "Point"
+
+        //.........................................................Создаем метод Выводящий содержимое списка "pList" на экран консоли.........................................
+
+        public void Drow()
+        {
+
+            foreach (Point element_spiska in pList)
+            {
+                element_spiska.Draww();
+            }
+        }
+        //.....................................................................................................................................................................
+
+
+    }
+}
