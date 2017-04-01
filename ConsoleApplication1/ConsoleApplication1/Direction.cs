576d8c07e038fa68f1ec8a82e57062b311ebbb5c
VladListing <chvch@mail.ru>
2017-04-01 13:57:41 +0700

    классы

---
+++ ConsoleApplication1/ConsoleApplication1/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    enum Direction //класс "Direction" (направление) список перечислений , он будет отвечать за направление движения змейки : в лево, в право, в верх , в низ.
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Foodcreator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Foodcreator  //класс  FoodCreator
+    {
+        //----------------------------------поля класса------------------------
+        int mapWidht;
+        int mapHeight;
+        char sym;
+
+        Random random = new Random();
+        //--------------------------------------------------------------------
+
+        //--------------------------------конструктор-----(метод в котором присваиваем значения полям класса)-------------------------
+        public  Foodcreator(int mapWidht, int mapHeight, char sym) //поля конструктора
+
+        {
+            this.mapWidht = mapWidht;     //присвоение полям класса  значений полей конструктора
+            this.mapHeight = mapHeight;  //там где приставка "this" это поля класса
+            this.sym = sym;             //
+        }
+        //----------------------------------------------------------------------------------------------------------------------------
+
+        //--------------------------------метод------------------------------------
+        public Point CreateFood() //метод который по итогам работы возвращает тип Point
+        {
+            int x = random.Next(2, mapWidht-2);
+            int y = random.Next(2, mapHeight - 2);
+
+            return new Point(x, y, sym);
+        }
+        //--------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    }
+}
